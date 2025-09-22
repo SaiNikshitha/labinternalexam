917ceadfd48b35b66b9aaebf4e77428e2a7d7dc1
SaiNikshitha <sainikshithamastha396@gmail.com>
2025-09-22 12:56:26 +0500

    logimprovement

--- FlightSearchController.java
+++ FlightSearchController.java
@@ -2,6 +2,7 @@ public class FlightSearchController {
     public static void main(String args[])
     {
         System.out.println("typo fix");
+        System.out.println("log improvement");
     }
     
 }
